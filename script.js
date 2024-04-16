cf7d16563fc8bb2a6b10778184832604632d3596
VishalV28 <vishalvich@gmail.com>
2024-04-15 23:17:50 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,9 @@ let isError = false;
 
 function cleanInputString(str) {
     const regex = /[+-\s]/;
-  }
+    return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+    const regex = /[0-9]e[0-9]/i;
+}
